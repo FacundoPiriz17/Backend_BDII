c5d9dc336d5fc769d43e3c9b8158295ea2357869
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2026-06-09 19:02:01 -0300

    Compra finalizado

--- Backend_BDII/Modules/Compras/DTOs/QrEntradaResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend_BDII.Modules.Compras.DTOs;
-
-public sealed class QrEntradaResponse
-{
-    public required int IdEntrada { get; init; }
-    public required string CodigoQr { get; init; }
-    public required string QrPngBase64 { get; init; }
-    public required DateTime FechaHoraGeneracion { get; init; }
-}
--- Backend_BDII/Modules/Compras/Models/NuevaEntradaCompra.cs
+++
@@ -1,8 +0,0 @@
-namespace Backend_BDII.Modules.Compras.Models;
-
-public sealed class NuevaEntradaCompra
-{
-    public required int IdPartido { get; init; }
-    public required string NombreSector { get; init; }
-    public required string CodigoQr { get; init; }
-}
